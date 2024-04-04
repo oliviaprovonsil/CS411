3c7ea305521237097ba900b8405eb1c6fe205cdb
Pranit Duddupudi <pranitd23@gmail.com>
2024-04-04 14:25:38 -0400

    has logo and text

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './TravelBuddyLogo.svg';
+import logoText from './TravelBuddyText.svg'
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+const OAuth2Data = require('./credentials.json');
+const client_ID = OAuth2Data.web.client_id;
+
 
 function App() {
+
+  <GoogleOAuthProvider clientId={client_ID}>...</GoogleOAuthProvider>;
+
+  const onSuccess = (res) => {
+    console.log("Login Successful: ", res);
+};
+const onFailure = (res) => {
+    console.log("Login Failure: ", res);
+}
   return (
     <div className="App">
       <header className="App-header">
+        <div className="logo-container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, This is our Weather Project!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logoText} className="App-logo-text" alt="logo" />
+        </div>
+        <div id="signInButton">
+            <GoogleLogin 
+            clientId={client_ID}
+            buttonText="Login"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={'single_host_origin'}
+            isSignedIn={true} />
+            </div>
       </header>
     </div>
   );
